9a05bb59cc4e8c57f0ec32b4b55c3accd232e45f
UPADHYAY-YATENDRA <yupadhyayyk@gmail.com>
2024-02-21 21:22:38 +0530

    Setting up basic code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Container from '@mui/material/Container';
+import ImageDashboard from './pages/ImageDashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    React.createElement(Container, { maxWidth: "sm" },
+      React.createElement(ImageDashboard, null)
+    )
   );
-}
+};
 
 export default App;
